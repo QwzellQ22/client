291c0d41650bc34bf06ca81675e9cceb49e02d0b
FILM <wzellxnext22@gmail.com>
2022-11-22 07:28:34 +0700

    upload

---
+++ src/Components/Register.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Components/Register.css";
+
+function Register() {
+  const [username, SetUsername] = useState("");
+  const [password, SetPassword] = useState("");
+  const [usereror, Seteror] = useState("");
+  const [psweror, Setpsw] = useState("");
+
+  const Login = () => {
+    //   Cek username
+    if (username === "") {
+      Seteror("Silahkan isi username anda");
+      // Cek Password
+    } else if (password === "") {
+      Setpsw("SIlahkan isi Passwordnya juga");
+    }
+  };
+  return (
+    <>
+      <div className="box">
+        <div className="container py-5">
+          <h1 className="text-muted"> Register</h1>
+          <p className="text-muted">Please Log in to autenticate</p>
+          <hr />
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => {
+                SetUsername(e.target.value);
+              }}
+            />
+            <p className=" text-danger p-2 m-3"> {usereror}</p>
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              onChange={(e) => {
+                SetPassword(e.target.value);
+              }}
+            />
+            <p className=" text-danger p-2 m-3 ">{psweror}</p>
+          </div>
+          <div className="form-group">
+            <label>NAMA</label>
+            <input type="text" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-primary mt-3" onClick={Login}>
+              Register
+            </button>
+          </div>
+          <p className="text-muted">
+            Dont have account? Please Click this <Link to="/">Create New Account</Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
